745c57cd67e964690b5a4ce660899cc8feeae3af
david <lostmorgan@gmail.com>
2022-08-25 12:45:34 -0400

    tailwind fix

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
+import "tailwindcss/tailwind.css";
 import Button from "./Button";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -13,10 +14,10 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const HelloWorld = Template.bind({});
+export const HelloVictor = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-HelloWorld.args = {
-  label: "Hello world!",
+HelloVictor.args = {
+  label: "Hello Victor!",
 };
 
 export const ClickMe = Template.bind({});
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import "./Button.css";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <button className="text-4xl text-red-800 bg-blue-500 rounded p-10">
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
